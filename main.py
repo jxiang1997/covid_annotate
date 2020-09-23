78d68751458531c7228cbdda5fb9ee9d26ee4f32
Justin Xiang <jxiang@Justins-MacBook-Pro.local>
2020-09-23 12:56:56 -0400

    m

--- main.py
+++ main.py
@@ -95,6 +95,8 @@ def main():
 
     last = request.args.get('last', 'NA')
 
+    index += 1
+
     return render_template("main.html", mols=mols, test_index=test_index, num_channels=NUM_CHANNELS, last=last)
 
 @app.route('/image/<path:path>')
